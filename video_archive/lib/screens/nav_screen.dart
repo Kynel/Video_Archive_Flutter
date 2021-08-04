05cd9c69b741042d3c1fbc577cd7a52f7fca67a4
kynel <kynel95@gmail.com>
2021-08-04 12:34:57 +0900

    implement video_card detail, state management with riverpod
    implement video_card detail, state management with riverpod

--- video_archive/lib/screens/nav_screen.dart
+++ video_archive/lib/screens/nav_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_youtube_ui/data.dart';
 import 'home_screen.dart';
+
+final selectedVideoProvider = StateProvider<Video?>((ref) => null);
 
 class NavScreen extends StatefulWidget {
   const NavScreen({Key? key}) : super(key: key);
@@ -23,17 +26,21 @@ class _NavScreenState extends State<NavScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-            children: _screens
-                .asMap()
-                .map(
-                  (i, screen) => MapEntry(
-                    i,
-                    Offstage(offstage: _selectedIndex != i, child: screen),
-                  ),
-                )
-                .values
-                .toList()),
+        body: Consumer(builder: (context, watch, _) {
+          final selectedVideo = watch(selectedVideoProvider).state;
+          print(selectedVideo);
+          return Stack(
+              children: _screens
+                  .asMap()
+                  .map(
+                    (i, screen) => MapEntry(
+                      i,
+                      Offstage(offstage: _selectedIndex != i, child: screen),
+                    ),
+                  )
+                  .values
+                  .toList());
+        }),
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           currentIndex: _selectedIndex,
--- video_archive/lib/widgets/video_card.dart
+++ video_archive/lib/widgets/video_card.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube_ui/screens/nav_screen.dart';
+import 'package:timeago/timeago.dart' as timeago;
 import 'package:flutter_youtube_ui/data.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class VideoCard extends StatelessWidget {
   final Video video;
@@ -11,34 +14,92 @@ class VideoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          children: [
-            Image.network(
-              video.thumbnailUrl,
-              height: 220.0,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            Positioned(
-              bottom: 8.0,
-              right: 8.0,
-              child: Container(
-                padding: const EdgeInsets.all(4.0),
-                color: Colors.black,
-                child: Text(
-                  video.duration,
-                  style: Theme.of(context)
-                      .textTheme
-                      .caption!
-                      .copyWith(color: Colors.white),
+    return GestureDetector(
+      onTap: () {
+        context.read(selectedVideoProvider).state = video;
+      },
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Image.network(
+                video.thumbnailUrl,
+                height: 220.0,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              Positioned(
+                bottom: 8.0,
+                right: 8.0,
+                child: Container(
+                  padding: const EdgeInsets.all(4.0),
+                  color: Colors.black,
+                  child: Text(
+                    video.duration,
+                    style: Theme.of(context)
+                        .textTheme
+                        .caption!
+                        .copyWith(color: Colors.white),
+                  ),
                 ),
               ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  onTap: () => print('Navigate to profile'),
+                  child: CircleAvatar(
+                    foregroundImage: NetworkImage(video.author.profileImageUrl),
+                  ),
+                ),
+                const SizedBox(width: 8.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          video.title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontSize: 15.0),
+                        ),
+                      ),
+                      Flexible(
+                        child: Text(
+                          '${video.author.username} • ${video.viewCount} views  ${timeago.format(video.timestamp)}',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .caption!
+                              .copyWith(fontSize: 14.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.more_vert,
+                    size: 20.0,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ],
+          )
+        ],
+      ),
     );
   }
 }
