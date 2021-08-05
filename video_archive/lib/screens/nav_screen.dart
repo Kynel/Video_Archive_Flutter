7ff8460b59dbd9b67af34f02ce761f0d7c69adec
kynel <kynel95@gmail.com>
2021-08-05 13:28:55 +0900

    implement video screen
    implement video screen

--- video_archive/lib/screens/nav_screen.dart
+++ video_archive/lib/screens/nav_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_youtube_ui/data.dart';
+import 'package:flutter_youtube_ui/screens/video_screen.dart';
 import 'package:miniplayer/miniplayer.dart';
 import 'home_screen.dart';
 
 final selectedVideoProvider = StateProvider<Video?>((ref) => null);
+
+final miniPlayerControllerProvider =
+    StateProvider.autoDispose<MiniplayerController>(
+  (ref) => MiniplayerController(),
+);
 
 class NavScreen extends StatefulWidget {
   const NavScreen({Key? key}) : super(key: key);
@@ -30,6 +36,8 @@ class _NavScreenState extends State<NavScreen> {
     return Scaffold(
         body: Consumer(builder: (context, watch, _) {
           final selectedVideo = watch(selectedVideoProvider).state;
+          final miniPlayerController =
+              watch(miniPlayerControllerProvider).state;
           return Stack(
             children: _screens
                 .asMap()
@@ -45,12 +53,14 @@ class _NavScreenState extends State<NavScreen> {
                     Offstage(
                       offstage: selectedVideo == null,
                       child: Miniplayer(
+                        controller: miniPlayerController,
                         minHeight: _playerMinHeight,
                         maxHeight: MediaQuery.of(context).size.height,
                         builder: (height, percentage) {
                           if (selectedVideo == null)
                             return const SizedBox.shrink();
-                          return Container(
+                          if (height <= _playerMinHeight + 50.0)
+                            return Container(
                               color: Theme.of(context).scaffoldBackgroundColor,
                               child: Column(
                                 children: [
@@ -59,16 +69,71 @@ class _NavScreenState extends State<NavScreen> {
                                       Image.network(selectedVideo.thumbnailUrl,
                                           height: _playerMinHeight - 4.0,
                                           width: 120.0,
-                                          fit: BoxFit.cover)
+                                          fit: BoxFit.cover),
+                                      Expanded(
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              Flexible(
+                                                child: Text(
+                                                  selectedVideo.title,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .caption!
+                                                      .copyWith(
+                                                          color: Colors.white,
+                                                          fontWeight:
+                                                              FontWeight.w500),
+                                                ),
+                                              ),
+                                              Flexible(
+                                                child: Text(
+                                                  '${selectedVideo.author.username}',
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .caption!
+                                                      .copyWith(
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                      ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      IconButton(
+                                        icon: const Icon(Icons.play_arrow),
+                                        onPressed: () {},
+                                      ),
+                                      IconButton(
+                                        icon: const Icon(Icons.close),
+                                        onPressed: () {
+                                          context
+                                              .read(selectedVideoProvider)
+                                              .state = null;
+                                        },
+                                      ),
                                     ],
                                   ),
                                   const LinearProgressIndicator(
                                     value: 0.4,
                                     valueColor: AlwaysStoppedAnimation<Color>(
-                                        Colors.red),
-                                  )
+                                      Colors.red,
+                                    ),
+                                  ),
                                 ],
-                              ));
+                              ),
+                            );
+                          return VideoScreen();
                         },
                       ),
                     ),
